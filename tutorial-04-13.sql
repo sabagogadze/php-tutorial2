53487c61da0c42a96e3ac322d805566769830da5
sabagogadze <37402948+sabagogadze@users.noreply.github.com>
2018-04-13 07:00:48 -0700

    Add files via upload

---
+++ tutorial-04-13.sql
@@ -0,0 +1,189 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 13, 2018 at 03:56 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `tutorial`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand`
+--
+
+CREATE TABLE `brand` (
+  `id` int(11) NOT NULL,
+  `brand` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `brand`
+--
+
+INSERT INTO `brand` (`id`, `brand`) VALUES
+(1, 'Wrangler'),
+(3, 'Levis');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `carusel image`
+--
+
+CREATE TABLE `carusel image` (
+  `id` int(11) NOT NULL,
+  `car_img` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `carusel image`
+--
+
+INSERT INTO `carusel image` (`id`, `car_img`) VALUES
+(1, 'images/headerlogo/slider1.jpeg'),
+(3, 'images/headerlogo/slider2.jpeg'),
+(4, 'images/headerlogo/slider1.jpeg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `category` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `perent` int(11) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `category`, `perent`) VALUES
+(1, 'Men', 0),
+(2, 'Women', 0),
+(3, 'Boys', 0),
+(4, 'Girls', 0),
+(5, 'Shirts', 1),
+(6, 'Pants', 1),
+(7, 'Shoes', 1),
+(8, 'Accesories', 1),
+(9, 'Shirts', 2),
+(10, 'Pants', 2),
+(11, 'Shoes', 2),
+(12, 'Dresses', 2),
+(13, 'Shirts', 3),
+(14, 'Pants', 3),
+(15, 'Shoes', 3),
+(16, 'Accesories', 3),
+(17, 'Shirts', 4),
+(18, 'Pants', 4),
+(19, 'Shoes', 4),
+(20, 'Dresses', 4),
+(21, 'Kids', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `list_price` decimal(10,2) NOT NULL,
+  `brand` int(11) NOT NULL,
+  `categories` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `image` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `featured` tinyint(4) NOT NULL DEFAULT '0',
+  `sizes` text COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `title`, `price`, `list_price`, `brand`, `categories`, `image`, `description`, `featured`, `sizes`) VALUES
+(1, 'Levi\'s jeans', '29.99', '39.99', 1, '6', '/ecomerce/images/products/men4.png', 'These jeans are amazing', 1, '28:3, 32:5, 36:1'),
+(2, 'Shirt', '19.99', '29.99', 1, '5', '/ecomerce/images/products/men1.png', 'Beatifull Shirt', 1, 'S:3, M:6, L:9');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brand`
+--
+ALTER TABLE `brand`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `carusel image`
+--
+ALTER TABLE `carusel image`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand`
+--
+ALTER TABLE `brand`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `carusel image`
+--
+ALTER TABLE `carusel image`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
