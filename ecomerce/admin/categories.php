027f5a6f3e0f30a4e3d6b46ffe82ac4754ef1296
sabagogadze <37402948+sabagogadze@users.noreply.github.com>
2018-04-18 07:01:18 -0700

    Add files via upload

--- ecomerce/admin/categories.php
+++ ecomerce/admin/categories.php
@@ -5,12 +5,60 @@
 	 require_once '../helpers/helpers.php';
 	 $sql = "SELECT * FROM categories WHERE parent = 0";
 	 $result = $db -> query($sql);
+	 $errors = array();
+
+	 //delete categories
+
+	 if (isset($_GET['delete']) && !empty($_GET['delete'])) {
+
+	 	
+	 	$delete_id = (int)$_GET['delete'];
+	 	
+		$delete_id = sanitize($delete_id);
+		var_dump($delete_id);
+
+	 	$sql_delete = "DELETE FROM categories WHERE id = '$delete_id'";
+	 	$db -> query($sql_delete);
+	 	header('Location: categories.php');
+	 }
+
+	 //process forms
+	 if (isset($_POST) && !empty($_POST)) {
+	 	$parent = sanitize($_POST['parent']);
+	 	$category = sanitize($_POST['category']);
+	 	$sqlForm = "SELECT * FROM categories WHERE category = '$category' AND parent = '$parent'";
+	 	$fresult =  $db ->query($sqlForm);
+	 	$count = mysqli_num_rows($fresult);
+	 	if ($category =="") {
+	 		$errors[].= 'The Category can not be empty';
+
+	 	} 
+	 	if ($count >0) {
+	 		$errors[].= $category. ' Already exists in database';
+	 	}
+
+	 	if (!empty($errors)) {
+	 		$display = display_arrors($errors); ?>
+	 		<script>
+	 			jQuery(document).ready(function() {
+	 			jQuery('#errors').html('<?php echo $display ?>')	
+	 			});
+	 		</script>
+	 		<?php 
+	 	} else {
+	 		$insertsql = "INSERT INTO categories (category, parent) VALUES ('$category', '$parent')";
+	 		$db -> query($insertsql);
+	 		header('Location: categories.php');
+	 	}
+	 }
 
 
  ?>
  	<h2 class="text-center">Categories</h2> <hr>
  	<div class="row">
 	 	<div class="col-md-6 col-xs-6 col-sm-6">
+	 		<legend class="col-md-6 col-xs-6 col-sm-6">Add Category</legend>
+	 		<div id="errors" ></div>
 	 		<form class="col-md-6" action="categories.php" method="post">
 	 			<div class="form-group">
 	 				<label for="parent">Parent</label>
@@ -22,6 +70,13 @@
 	 					<?php endwhile;  ?>
 	 				</select>
 	 				
+	 			</div>
+	 			<div class="form-group">
+	 				<label for="category">Category</label>
+	 				<input type="text" class="form-control" name="category" id="category">
+	 			</div>
+	 			<div class="form-group">
+	 				<input type="submit" value="Add Category" class="btn btn-success">
 	 			</div>
 	 			
 	 		</form>
@@ -48,7 +103,7 @@
 		 					<td>parent</td>
 		 					<td>
 		 						<a href="categories.php?edit=<?php echo $categories['id'] ?>"  class="btn btn-success btn-sm"><span>Edit</span></a>
-		 						<a href="categories.php?delte=<?php echo $categories['id'] ?>"  class="btn btn-success btn-sm"><span>Edit</span></a>
+		 						<a href="categories.php?delete=<?php echo $categories['id'] ?>"  class="btn btn-success btn-sm"><span>delete</span></a>
 		 					</td>
 		 					
 		 				</tr>
@@ -57,8 +112,8 @@
 		 					<td><?php echo $child['category'] ?></td>
 		 					<td><?php echo $categories['category'] ?></td>
 		 					<td>
-		 						<a href="categories.php?edit=<?php echo $categories['id'] ?>"  class="btn btn-success btn-sm"><span>Edit</span></a>
-		 						<a href="categories.php?delte=<?php echo $categories['id'] ?>"  class="btn btn-success btn-sm"><span>Edit</span></a>
+		 						<a href="categories.php?edit=<?php echo $child['id'] ?>"  class="btn btn-success btn-sm"><span>Edit</span></a>
+		 						<a href="categories.php?delete=<?php echo $child['id'] ?>"  class="btn btn-success btn-sm"><span>delete</span></a>
 		 					</td>
 		 				</tr>
 		 				<?php endwhile;  ?>	
--- ecomerce/includes/navigation.php
+++ ecomerce/includes/navigation.php
@@ -1,5 +1,5 @@
 <?php
-  $psql = "SELECT * FROM `categories` WHERE perent = 0";
+  $psql = "SELECT * FROM `categories` WHERE parent = 0";
     $pquery = $db -> query($psql);
 ?>
 <nav class="navbar  navbar-expand-lg navbar-light bg-light">
@@ -9,15 +9,15 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
-        <?php while ($perent = mysqli_fetch_assoc($pquery)): ?>
+        <?php while ($parent = mysqli_fetch_assoc($pquery)): ?>
         <?php 
-          $perent_id = $perent['id'];
-          $chsql = "SELECT * FROM `categories` WHERE perent ='$perent_id'"; 
+          $parent_id = $parent['id'];
+          $chsql = "SELECT * FROM `categories` WHERE parent ='$parent_id'"; 
           $chquery = $db -> query($chsql);
         ?>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-         <?php echo $perent['category'] ?>
+         <?php echo $parent['category'] ?>
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
           <?php while ($child = mysqli_fetch_assoc($chquery)): ?>
